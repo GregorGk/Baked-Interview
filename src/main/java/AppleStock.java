800f0551531461f972cfecd196d1eec8f67857c4
GregorGk <gorkiewicz.grzegorz@gmail.com>
2017-12-18 20:43:22 +0100

    Add an implementation of a greedy algorithm

--- src/main/java/AppleStock.java
+++ src/main/java/AppleStock.java
@@ -1,6 +1,19 @@
 class AppleStock {
 
+  /**
+   * Returns the maximum of possible profits from yesterday's stocks.
+   * Can be negative.
+   */
   public int getMaxProfit(int[] stockPricesYesterday) {
-    return 0;
+    if (stockPricesYesterday.length < 2) {
+      throw new IllegalArgumentException("At least two stock prices required.");
+    }
+    int currentMin = stockPricesYesterday[0];
+    int currentProfit = stockPricesYesterday[1] - stockPricesYesterday[0];
+    for (int i = 2; i < stockPricesYesterday.length; i++) {
+      currentProfit = Math.max(stockPricesYesterday[i] - currentMin, currentProfit);
+      currentMin = Math.min(stockPricesYesterday[i], currentMin);
+    }
+    return currentProfit;
   }
 }
