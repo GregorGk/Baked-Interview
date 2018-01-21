88375131db187b79b7338a142d1f012caa97e7cc
GregorGk <gorkiewicz.grzegorz@gmail.com>
2018-01-21 21:41:19 +0100

    Add another failing tests for TDD

---
+++ src/main/java/ProductOfAllOther.java
@@ -0,0 +1,12 @@
+public class ProductOfAllOther {
+
+  /**
+   * Without using division.
+   *
+   * @param inputArray product of all elements of this array is assumed to produce no overflow.
+   * @return an array containing products of all other elements.
+   */
+  public int[] productOfAllOtherNumbers(int[] inputArray) {
+    return null;
+  }
+}
---
+++ src/test/java/ProductOfAllOtherTest.java
@@ -0,0 +1,33 @@
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+public class ProductOfAllOtherTest {
+
+  @Rule
+  public ErrorCollector errorCollector;
+  private ProductOfAllOther productOfAllOther;
+
+  public ProductOfAllOtherTest() {
+    this.productOfAllOther = new ProductOfAllOther();
+    this.errorCollector = new ErrorCollector();
+  }
+
+  @Test
+  public void checkProductOfAllOther() {
+    Map<int[], int[]> testCases = new LinkedHashMap<>();
+    testCases.put(new int[] {1, 7, 3, 4}, new int[] {84, 12, 28, 21});
+    testCases.put(new int[] {0, 1, 2, 3}, new int[]{6, 0, 0, 0});
+    testCases.put(new int[] {1, 0, 0, 1}, new int[]{0, 0, 0, 0});
+    testCases.forEach((actualInput, expectedOutput)
+        -> errorCollector.checkThat(
+        this.productOfAllOther
+            .productOfAllOtherNumbers(actualInput), equalTo(expectedOutput)));
+  }
+}
